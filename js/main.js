364345861eb194d7b90f0b6dbd441f6e953da815
Matthew Reddick <matthewjreddick@gmail.com>
2014-11-25 19:29:59 -0800

    refactored JS code to use OOP and not create any globals

--- js/main.js
+++ js/main.js
@@ -1,37 +1,45 @@
-evalString = "";
+(function(){
 
-var numButtons = document.getElementsByClassName("digit-button");
-var mathButtons = document.getElementsByClassName("math-symbol");
-var output = document.getElementById("output");
-var symbols = {divide: "/", multiply: "*", plus: "+", minus: "-"}
-var clear = document.getElementById("clear");
-var enter = document.getElementById("enter");
+	function Calculator(){
+		
+		//var self = this;
+		var evalString = "";
 
-function updateOutput(str) {
-	output.innerHTML = str;
-}
+		var numButtons = document.getElementsByClassName("digit-button");
+		var mathButtons = document.getElementsByClassName("math-symbol");
+		var output = document.getElementById("output");
+		var symbols = {divide: "/", multiply: "*", plus: "+", minus: "-"}
+		var clear = document.getElementById("clear");
+		var enter = document.getElementById("enter");
 
-for(var i = 0; i < numButtons.length; i++) {
-	numButtons[i].addEventListener("click", function() {
-		evalString += this.id.split("-")[1];
-		updateOutput(evalString);
-	});
-}
+		function updateOutput(str) {
+			output.innerHTML = str;
+		}
 
-for(var i = 0; i < mathButtons.length; i++) {
-	mathButtons[i].addEventListener("click", function() {
-		evalString += symbols[this.id];
-		updateOutput(evalString);
-	});
-}
+		for(var i = 0; i < numButtons.length; i++) {
+			numButtons[i].addEventListener("click", function() {
+				evalString += this.id.split("-")[1];
+				updateOutput(evalString);
+			});
+		}
 
-clear.addEventListener("click", function() {
-	evalString = "";
-	updateOutput(evalString);
-});
+		for(var i = 0; i < mathButtons.length; i++) {
+			mathButtons[i].addEventListener("click", function() {
+				evalString += symbols[this.id];
+				updateOutput(evalString);
+			});
+		}
 
-enter.addEventListener("click", function() {
-	answer = eval(evalString);
-	updateOutput(answer);
-	evalString = "";
-});
+		clear.addEventListener("click", function() {
+			evalString = "";
+			updateOutput(evalString);
+		});
+
+		enter.addEventListener("click", function() {
+			answer = eval(evalString);
+			updateOutput(answer);
+			evalString = "";
+		});
+	};
+	var CALC = new Calculator();
+})();
